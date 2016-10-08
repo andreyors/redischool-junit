a5ebce09f0b3c9e66295e909f31a28edad0dfb38
Andrey Orsoev <andrey.orsoev@gmail.com>
2016-10-08 20:25:46 +0200

    Fix

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,4 +1,3 @@
-
 public class Calculator {
 
     public Double sum(Double left, Double right) {
--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,5 +1,5 @@
 import org.junit.Test;
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 public class CalculatorTest {
 
@@ -8,7 +8,7 @@ public class CalculatorTest {
         Calculator calculator = new Calculator();
 
         Double actual = calculator.sum(4., 5.);
-        Double expected = new Double(9);
+        Double expected = 9.;
 
         assertEquals(expected, actual);
     }
@@ -18,7 +18,7 @@ public class CalculatorTest {
         Calculator calculator = new Calculator();
 
         Double actual = calculator.sum(-9., 10.);
-        Double expected = new Double(1);
+        Double expected = 1.;
 
         assertEquals(expected, actual);
     }
@@ -28,12 +28,12 @@ public class CalculatorTest {
         Calculator calculator = new Calculator();
 
         Double actual = calculator.sum(0., 0.);
-        Double expected = new Double(0);
+        Double expected = 0.;
 
         assertEquals(expected, actual);
     }
 
-    @Test (expected = NullPointerException.class)
+    @Test(expected = NullPointerException.class)
     public void testShouldThrowException() {
         Calculator calculator = new Calculator();
 
